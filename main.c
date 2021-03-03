499d842e29607c90da317f27e11d30ae60adaba0
silan-liu <413769707@qq.com>
2021-03-03 09:04:37 +0800

    add builtin cmd

--- main.c
+++ main.c
@@ -130,11 +130,29 @@ int lsh_num_builtions()
 
 int lsh_cd(char **args)
 {
+  if (args[1] == NULL)
+  {
+    fprintf(stderr, "lsh: no args");
+  }
+  else
+  {
+    if (chdir(args[1]) != 0)
+    {
+      perror("chdir error!");
+    }
+  }
   return 0;
 }
 
 int lsh_help(char **args)
 {
+  printf("lsh builtin cmd:");
+
+  for (int i = 0; i < lsh_num_builtions(); i++)
+  {
+    printf("  %s\n", builtin_str[i]);
+  }
+
   return 0;
 }
 
